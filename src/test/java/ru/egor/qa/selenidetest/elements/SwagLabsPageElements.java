2103ef8966b87ec877128c2c4e020b2d9300ebf6
EgorBezrukov <poladez@yandex.ru>
2023-04-09 11:26:23 +0300

    Allure + feature

--- src/test/java/ru/egor/qa/selenidetest/elements/SwagLabsPageElements.java
+++ src/test/java/ru/egor/qa/selenidetest/elements/SwagLabsPageElements.java
@@ -1,4 +1,5 @@
 package ru.egor.qa.selenidetest.elements;
+
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
@@ -8,7 +9,11 @@ public class SwagLabsPageElements {
 
     protected final SelenideElement login = $("#user-name");
     protected final SelenideElement password = $("#password");
-    protected final SelenideElement loginButton = $("#login-button");
     protected final ElementsCollection inventoryList = $$(".inventory_list");
-
+    protected final SelenideElement backPackOpenPage = $("#item_4_title_link");
+    protected final SelenideElement backPackPrice = $(".inventory_details_price");
+    protected final SelenideElement addToCartButton = $("#add-to-cart-sauce-labs-backpack");
+    protected final SelenideElement cartBadge = $(".shopping_cart_badge");
+    protected final SelenideElement cartButton = $(".shopping_cart_link");
+    protected final SelenideElement itemPrise = $(".inventory_item_price");
 }
--- src/test/java/ru/egor/qa/selenidetest/steps/SwagLabsStep.java
+++ src/test/java/ru/egor/qa/selenidetest/steps/SwagLabsStep.java
@@ -1,26 +1,55 @@
 package ru.egor.qa.selenidetest.steps;
-import com.codeborne.selenide.ElementsCollection;
+
 import io.cucumber.java.ru.И;
 import io.cucumber.java.ru.Когда;
 import io.cucumber.java.ru.Тогда;
 import org.junit.jupiter.api.Assertions;
-import org.openqa.selenium.Keys;
-import ru.egor.qa.selenidetest.elements.KlavaGonkiPageElements;
 import ru.egor.qa.selenidetest.elements.SwagLabsPageElements;
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
+import static com.codeborne.selenide.Condition.text;
 
 public class SwagLabsStep extends SwagLabsPageElements {
-        @Когда("пользователь выполняет вход")
-        public void inputLogin(){
-                login.setValue("");
-                password.setValue("").pressEnter();
-                loginButton.click();
-        }
+    @Когда("пользователь выполняет вход")
+    public void inputLogin() {
+        login.setValue("standard_user");
+        password.setValue("secret_sauce").pressEnter();
+    }
 
-        @И("Он проверяет что количество элементов на странице ровно {int}")
-        public void checkElementPage(int quantity) {
-                inventoryList.shouldHave(sizeGreaterThanOrEqual(quantity));
-        }
+    @И("Он проверяет что количество элементов на странице ровно {int}")
+    public void checkElementPage(int quantity) {
+        inventoryList.shouldHave(sizeGreaterThanOrEqual(quantity))
+                .first().shouldHave(text("Sauce Labs Backpack"));
+
+    }
+
+    @И("Он переходит на страницу рюкзака и запоминает значение цены")
+    public String openBackPackPage() {
+        backPackOpenPage.click();
+        final String price = backPackPrice.getText();
+        return price;
+    }
+
+    @И("Он добавляет товар в корзину и проверяет что в корзине отображается колличесво товара {int}")
+    public void addToShoppingCart(int quantity) {
+        addToCartButton.click();
+        String result = cartBadge.getText();
+        Integer res = Integer.parseInt(result);
+        Assertions.assertEquals(quantity, res);
+//        if (res.equals(quantity)){
+//            System.out.println(true);
+//        }else  System.out.println(false);
+
+    }
+
+    @Тогда("Он переходит в корзину и проверят сумму заказа")
+    public void checkSumOrder() {
+
+        cartButton.click();
+        String result = itemPrise.getText();
+//        Assertions.assertEquals(result, openBackPackPage());
+        if (result.equals(openBackPackPage())) {
+            System.out.println(result);
+        }else  System.out.println(result + " не равно " + openBackPackPage());
+    }
 }
